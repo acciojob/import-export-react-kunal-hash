92299ed31cb4f88de937fbe1596ae68539b8ed99
kunal-hash <kunalpardhi7319@gmail.com>
2023-03-24 02:51:19 +0530

    jai shree ram

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import InputQuery from "./InputQuery";
+import SubHeading from "./SubHeading";
+import Heading from "./Heading";
+import SubmitButton from "./SubmitButton";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      < Heading/>
+      < InputQuery/>
+      < SubHeading/>
+      < SubmitButton/>
     </div>
   )
 }
